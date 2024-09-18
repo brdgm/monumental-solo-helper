eb478f19318e77f411460ab142a8ca15d146c931
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-09-18 21:10:57 +0200

    additional action help

--- src/services/ActionsHelp.ts
+++ src/services/ActionsHelp.ts
@@ -8,20 +8,32 @@ const mappings = [
     Action.KNOWLEDGE_TAKE_LOWEST_COST,
     Action.KNOWLEDGE_TAKE_HIGHEST_COST,
     Action.KNOWLEDGE_TAKE_ALL,
+    Action.DRAW_RESERVE_CARD_3_GAIN_KNOWLEDGE
   ]},
   { help: ActionHelp.BUILD_WONDER, actions: [
     Action.WONDER_COMPLETE_ACQUIRE_LOWEST_COST,
+    Action.FEWEST_WONDERS_WONDER_COMPLETE_ACQUIRE_GAIN_BUILDING_LOSTEST_COST,
+    Action.DRAW_RESERVE_CARD_2_GAIN_WONDERS
   ]},
   { help: ActionHelp.CONQUER_PROVINCE, actions: [
     Action.CONQUER_1_ADJACENT_LOWEST_COST,
     Action.CONQUER_1_ADJACENT_HIGHEST_COST,
     Action.CONQUER_3_ADJACENT_HIGHEST_COST,
+    Action.FEWEST_PROVINCES_CONQUER_1_ADJACENT_LOWEST_COST
   ]},
   { help: ActionHelp.MOVE_EXPLORERS, actions: [
     Action.EXPLORER_BOTH_MOVE_1_SPACE,
     Action.EXPLORER_BOTH_MOVE_2_SPACE,
     Action.EXPLORER_BOTH_MOVE_3_SPACE
   ]},
+  { help: ActionHelp.TRADE_TRACK, actions: [
+    Action.TRADE_TRACK_1_STEP,
+    Action.TRADE_TRACK_2_STEP,
+    Action.TRADE_TRACK_LAST_TRADE_TRACK_2_STEP
+  ]},
+  { help: ActionHelp.HERO, actions: [
+    Action.TAKE_RETURN_HERO
+  ]}
 ]
 
 const actionMap = new Map<Action,ActionHelp>()
