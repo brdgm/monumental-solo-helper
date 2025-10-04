ebca88b59895463e25c83bc755a48f250e1d7902
Stefan Seifert <stefanseifert@users.noreply.github.com>
2025-10-04 13:18:56 +0200

    eliminate code warnings

--- src/util/toCardNames.ts
+++ src/util/toCardNames.ts
@@ -4,6 +4,6 @@ import CardName from '@/services/enum/CardName'
 /**
  * Converts list of card instances to card names.
  */
-export default function(cards : Card[]) : CardName[] {
+export default function toCardNames(cards : Card[]) : CardName[] {
   return cards.map(card => card.name)
 }
